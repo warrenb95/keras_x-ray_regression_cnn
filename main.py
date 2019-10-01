7d2a5b375ac1118b53c11a32fab25c2884ab00da
warren <warrenb95@live.co.uk>
2019-10-01 10:10:36 +0100

    added gitignore and imports

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers.core import Dense, Flatten
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import *
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
+import itertools
+
+def main():
+    print('hello world')
+
+if __name__ == "__main__":
+    main()
