ff22708035a11fb550f5668de1523031e1030a28
warren <warrenb95@live.co.uk>
2019-10-04 11:52:53 +0100

    Update sortdata.py
    Seprate into limb/body part

--- sortdata.py
+++ sortdata.py
@@ -3,10 +3,21 @@ import os
 import shutil
 import typing
 
-train_normal_folder = 'dataset/train/normal'
-train_abnormal_folder = 'dataset/train/abnormal'
-valid_normal_folder = 'dataset/valid/normal'
-valid_abnormal_folder = 'dataset/valid/abnormal'
+train_elbow_folder = 'dataset/train/elbow'
+train_finger_folder = 'dataset/train/finger'
+train_forearm_folder = 'dataset/train/forearm'
+train_hand_folder = 'dataset/train/hand'
+train_humerus_folder = 'dataset/train/humerus'
+train_shoulder_folder = 'dataset/train/shoulder'
+train_wrist_folder = 'dataset/train/wrist'
+
+valid_elbow_folder = 'dataset/valid/elbow'
+valid_finger_folder = 'dataset/valid/finger'
+valid_forearm_folder = 'dataset/valid/forearm'
+valid_hand_folder = 'dataset/valid/hand'
+valid_humerus_folder = 'dataset/valid/humerus'
+valid_shoulder_folder = 'dataset/valid/shoulder'
+valid_wrist_folder = 'dataset/valid/wrist'
 
 image_count = 0
 
@@ -18,6 +29,8 @@ def copy_files(src: str , dest: str):
         if os.path.isfile(full_file_name):
             shutil.copy(full_file_name, dest + '/' + str(image_count) + '.png')
             image_count += 1
+        else:
+            print("Copy failed :\(")
 
 training_file_name = 'MURA-v1.1/train_labeled_studies.csv'
 valid_file_name = 'MURA-v1.1/valid_labeled_studies.csv'
@@ -28,10 +41,20 @@ with open(training_file_name) as csv_file:
     row_count = 1
 
     for row in csv_reader:
-        if row[1] == '0':
-            copy_files(row[0], train_normal_folder)
-        else:
-            copy_files(row[0], train_abnormal_folder)
+        if 'elbow' in row[0]:
+            copy_files(row[0], train_elbow_folder)
+        elif 'finger' in row[0]:
+            copy_files(row[0], train_finger_folder)
+        elif 'forearm' in row[0]:
+            copy_files(row[0], train_forearm_folder)
+        elif 'hand' in row[0]:
+            copy_files(row[0], train_hand_folder)
+        elif 'humerus' in row[0]:
+            copy_files(row[0], train_humerus_folder)
+        elif 'shoulder' in row[0]:
+            copy_files(row[0], train_shoulder_folder)
+        elif 'wrist' in row[0]:
+            copy_files(row[0], train_wrist_folder)
 
         print('Training: ', row_count)
         row_count += 1
@@ -42,10 +65,20 @@ with open(valid_file_name) as csv_file:
     row_count = 1
 
     for row in csv_reader:
-        if row[1] == '0':
-            copy_files(row[0], valid_normal_folder)
-        else:
-            copy_files(row[0], valid_abnormal_folder)
+        if 'elbow' in row[0]:
+            copy_files(row[0], valid_elbow_folder)
+        elif 'finger' in row[0]:
+            copy_files(row[0], valid_finger_folder)
+        elif 'forearm' in row[0]:
+            copy_files(row[0], valid_forearm_folder)
+        elif 'hand' in row[0]:
+            copy_files(row[0], valid_hand_folder)
+        elif 'humerus' in row[0]:
+            copy_files(row[0], valid_humerus_folder)
+        elif 'shoulder' in row[0]:
+            copy_files(row[0], valid_shoulder_folder)
+        elif 'wrist' in row[0]:
+            copy_files(row[0], valid_wrist_folder)
 
         print('Valid: ', row_count)
         row_count += 1
