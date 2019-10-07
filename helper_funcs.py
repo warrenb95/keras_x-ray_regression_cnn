77f25c46c0ab8f33a374f84381037b37fa825358
Warren <warrenb95@live.co.uk>
2019-10-07 11:26:23 +0100

    Create  new model func in helper_func

--- helper_funcs.py
+++ helper_funcs.py
@@ -1,3 +1,13 @@
+import keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers.core import Dense, Flatten, Dropout
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import *
 from keras.models import model_from_json
 
 def save_model(model):
@@ -20,3 +30,34 @@ def load_model():
     print("Loaded model from disk")
 
     return loaded_model
+
+def create_new_model():
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 4)))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Dropout(0.2))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Dropout(0.2))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Dropout(0.2))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(Conv2D(32, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Dropout(0.2))
+    model.add(Dense(256, activation='softmax'))
+    model.add(Flatten())
+    model.add(Dense(7, activation='softmax'))
+
+    return model
--- main.py
+++ main.py
@@ -8,12 +8,11 @@ from keras.optimizers import Adam
 from keras.metrics import categorical_crossentropy
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import *
 from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
 import itertools
 
-from helper_funcs import save_model
+import helper_funcs
 
 train_path = 'dataset/train'
 valid_path = 'dataset/valid'
@@ -21,39 +20,14 @@ valid_path = 'dataset/valid'
 train_batches = ImageDataGenerator().flow_from_directory(train_path, color_mode='rgba', batch_size=30)
 valid_batches = ImageDataGenerator().flow_from_directory(valid_path, color_mode='rgba', batch_size=30)
 
-model = Sequential()
-
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 4)))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Dropout(0.2))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Dropout(0.2))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Dropout(0.2))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Dropout(0.2))
-model.add(Dense(256, activation='softmax'))
-model.add(Flatten())
-model.add(Dense(7, activation='softmax'))
+model = helper_funcs.create_new_model()
+# model = helper_funcs.load_model()
 
 model.compile(Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
 
 try:
     model.fit_generator(train_batches, steps_per_epoch=1227, validation_data=valid_batches, validation_steps=106, epochs=5, verbose=1)
 except KeyboardInterrupt:
-    save_model(model)
+    helper_funcs.save_model(model)
 else:
-    save_model(model)
+    helper_funcs.save_model(model)
