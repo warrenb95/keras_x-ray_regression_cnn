c958d3d53b1c65e25d3ab180f74375fafb202cdc
Warren <warrenb95@live.co.uk>
2019-11-04 12:26:57 +0000

    Update main.py
    Starting to think about how to launch the different parts of the application.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from controller import Controller
 
 if __name__ == "__main__":
-    controller = Controller()
-    controller.run()
+
+    print(
+        "1. Launch the GUI application.\n"
+        "2. Train Classificatioin network"
+    )
+
+    usr_in = input("Please select: ")
+
+    if int(usr_in) == 1:
+        controller = Controller()
+        controller.run()
+    elif int(usr_in) == 2:
+        # Todo
+        pass
