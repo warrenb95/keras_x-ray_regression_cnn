61a6b8304876d3cc3c78be22b46e4ba14f08a169
warren <warrenb95@live.co.uk>
2019-11-07 09:48:22 +0000

    Update sortdata.py
    Create a dataset.csv file with the correct values for regression (0 || 1).

--- sortdata.py
+++ sortdata.py
@@ -21,16 +21,25 @@ valid_wrist_folder = 'dataset/valid/wrist'
 
 image_count = 0
 
-def copy_files(src: str , dest: str):
+def copy_files(src: str , dest: str, value: int):
     global image_count
     src_files = os.listdir(src)
     for file_name in src_files:
         full_file_name = os.path.join(src, file_name)
+        new_file_name = dest + '/' + str(image_count) + '.png'
         if os.path.isfile(full_file_name):
-            shutil.copy(full_file_name, dest + '/' + str(image_count) + '.png')
+            shutil.copy(full_file_name, new_file_name)
             image_count += 1
+
+            add_to_dataset_file(new_file_name, value)
         else:
             print("Copy failed :\(")
+
+
+def add_to_dataset_file(path: str, value: int):
+    with open('dataset/dataset.csv', 'a') as dataset_file:
+        dataset_file.write(path + "," + str(value) + '\n')
+
 
 training_file_name = 'MURA-v1.1/train_labeled_studies.csv'
 valid_file_name = 'MURA-v1.1/valid_labeled_studies.csv'
@@ -42,19 +51,47 @@ with open(training_file_name) as csv_file:
 
     for row in csv_reader:
         if 'ELBOW' in row[0]:
-            copy_files(row[0], train_elbow_folder)
+            # copy_files(row[0], train_elbow_folder)
+            if row[1] == '1':
+                copy_files(row[0], train_elbow_folder, 1)
+            else:
+                copy_files(row[0], train_elbow_folder, 0)
         elif 'FINGER' in row[0]:
-            copy_files(row[0], train_finger_folder)
+            # copy_files(row[0], train_finger_folder)
+            if row[1] == '1':
+                copy_files(row[0], train_finger_folder, 1)
+            else:
+                copy_files(row[0], train_finger_folder, 0)
         elif 'FOREARM' in row[0]:
-            copy_files(row[0], train_forearm_folder)
+            # copy_files(row[0], train_forearm_folder)
+            if row[1] == '1':
+                copy_files(row[0], train_forearm_folder, 1)
+            else:
+                copy_files(row[0], train_forearm_folder, 0)
         elif 'HAND' in row[0]:
-            copy_files(row[0], train_hand_folder)
+            # copy_files(row[0], train_hand_folder)
+            if row[1] == '1':
+                copy_files(row[0], train_hand_folder, 1)
+            else:
+                copy_files(row[0], train_hand_folder, 0)
         elif 'HUMERUS' in row[0]:
-            copy_files(row[0], train_humerus_folder)
+            # copy_files(row[0], train_humerus_folder)
+            if row[1] == '1':
+                copy_files(row[0], train_humerus_folder, 1)
+            else:
+                copy_files(row[0], train_humerus_folder, 0)
         elif 'SHOULDER' in row[0]:
-            copy_files(row[0], train_shoulder_folder)
+            # copy_files(row[0], train_shoulder_folder)
+            if row[1] == '1':
+                copy_files(row[0], train_shoulder_folder, 1)
+            else:
+                copy_files(row[0], train_shoulder_folder, 0)
         elif 'WRIST' in row[0]:
-            copy_files(row[0], train_wrist_folder)
+            # copy_files(row[0], train_wrist_folder)
+            if row[1] == '1':
+                copy_files(row[0], train_wrist_folder, 1)
+            else:
+                copy_files(row[0], train_wrist_folder, 0)
         else:
             print("Oops")
 
@@ -68,19 +105,47 @@ with open(valid_file_name) as csv_file:
 
     for row in csv_reader:
         if 'ELBOW' in row[0]:
-            copy_files(row[0], valid_elbow_folder)
+            # copy_files(row[0], valid_elbow_folder)
+            if row[1] == '1':
+                copy_files(row[0], valid_elbow_folder, 1)
+            else:
+                copy_files(row[0], valid_elbow_folder, 0)
         elif 'FINGER' in row[0]:
-            copy_files(row[0], valid_finger_folder)
+            # copy_files(row[0], valid_finger_folder)
+            if row[1] == '1':
+                copy_files(row[0], valid_finger_folder, 1)
+            else:
+                copy_files(row[0], valid_finger_folder, 0)
         elif 'FOREARM' in row[0]:
-            copy_files(row[0], valid_forearm_folder)
+            # copy_files(row[0], valid_forearm_folder)
+            if row[1] == '1':
+                copy_files(row[0], valid_forearm_folder, 1)
+            else:
+                copy_files(row[0], valid_forearm_folder, 0)
         elif 'HAND' in row[0]:
-            copy_files(row[0], valid_hand_folder)
+            # copy_files(row[0], valid_hand_folder)
+            if row[1] == '1':
+                copy_files(row[0], valid_hand_folder, 1)
+            else:
+                copy_files(row[0], valid_hand_folder, 0)
         elif 'HUMERUS' in row[0]:
-            copy_files(row[0], valid_humerus_folder)
+            # copy_files(row[0], valid_humerus_folder)
+            if row[1] == '1':
+                copy_files(row[0], valid_humerus_folder, 1)
+            else:
+                copy_files(row[0], valid_humerus_folder, 0)
         elif 'SHOULDER' in row[0]:
-            copy_files(row[0], valid_shoulder_folder)
+            # copy_files(row[0], valid_shoulder_folder)
+            if row[1] == '1':
+                copy_files(row[0], valid_shoulder_folder, 1)
+            else:
+                copy_files(row[0], valid_shoulder_folder, 0)
         elif 'WRIST' in row[0]:
-            copy_files(row[0], valid_wrist_folder)
+            # copy_files(row[0], valid_wrist_folder)
+            if row[1] == '1':
+                copy_files(row[0], valid_wrist_folder, 1)
+            else:
+                copy_files(row[0], valid_wrist_folder, 0)
 
         print('Valid: ', row_count)
         row_count += 1
