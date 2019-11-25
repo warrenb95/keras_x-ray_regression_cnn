cfba9dc547702e4b564f4d71c3000ad3a3df4032
warren <warrenb95@live.co.uk>
2019-11-25 13:49:58 +0000

    Revert "Revert "Merge branch 'master' into regression_models""
    This reverts commit d92d6f9cb3286390b0fff3af1313fdaa68aa4649.

--- view.py
+++ view.py
@@ -1,7 +1,8 @@
 import tkinter as tk
+from PIL import ImageTk, Image
 class View():
 
-    def __init__(self, master):
+    def __init__(self, root):
         print("init view")
 
         self.result_val = 0.0
@@ -9,17 +10,27 @@ class View():
 
         self.file_list = []
 
-        self.make_image_frame(master)
-        self.make_side_frame(master)
+        self.make_image_frame(root)
+        self.make_side_frame(root)
+        self.add_tmp_img()
 
-    def make_image_frame(self, master):
-        master.update()
-        self.img_frame = tk.Frame(master, width = master.winfo_width() * 0.60, height = master.winfo_height(), bg = "grey")
-        self.img_frame.pack(side = tk.LEFT)
+    def make_image_frame(self, root):
+        root.update()
 
-    def make_side_frame(self, master):
-        master.update()
-        self.side_frame = tk.Frame(master, width = master.winfo_width() * 0.40, height = master.winfo_height(), bg = "white")
+        self.image_canvas = tk.Canvas(root, width = root.winfo_width() * 0.60, height = root.winfo_height())
+        self.image_canvas.pack(side = tk.LEFT)
+
+    def add_tmp_img(self):
+        self.img = ImageTk.PhotoImage(Image.open("temp_image.png").resize((self.image_canvas.winfo_width(), self.image_canvas.winfo_height()), Image.ANTIALIAS))
+        self.image_on_canvas =  self.image_canvas.create_image(0, 0, anchor=tk.NW, image=self.img)
+
+    def set_image(self, image_path):
+        self.img = ImageTk.PhotoImage(Image.open(image_path).resize((self.image_canvas.winfo_width(), self.image_canvas.winfo_height()), Image.ANTIALIAS))
+        self.image_canvas.itemconfig(self.image_on_canvas, image=self.img)
+
+    def make_side_frame(self, root):
+        root.update()
+        self.side_frame = tk.Frame(root, width = root.winfo_width() * 0.40, height = root.winfo_height(), bg = "white")
         self.side_frame.pack(side = tk.LEFT, fill = tk.BOTH, expand = True)
 
         self.make_file_frame(self.side_frame)
@@ -27,16 +38,34 @@ class View():
         self.result_label = tk.Label(self.side_frame, text = self.result_str, font = "40 40", fg = "#ff5c33", borderwidth = 2, relief = "solid")
         self.result_label.pack(fill = tk.X, pady = 75, padx = 5)
 
-        self.process_btn = tk.Button(self.side_frame, text = "Process X-Ray", font = "18")
-        self.process_btn.pack(pady = 25, padx = 5, side = "bottom")
+        self.make_btn_frame(self.side_frame)
 
-    def make_file_frame(self, master):
-        master.update()
-        self.file_frame = tk.Frame(master, width = master.winfo_width(), height = master.winfo_height() * 0.3, bg = "grey")
+    def make_file_frame(self, root):
+        root.update()
+        self.file_frame = tk.Frame(root, width = root.winfo_width(), height = root.winfo_height() * 0.3, bg = "grey")
         self.file_frame.pack(fill = tk.X, padx = 10, pady = 10)
 
+        self.upload_btn = tk.Button(self.file_frame, width = 5, height = 2, text = "Upload", font = "10", bg = "#70db70")
+        self.upload_btn.grid(row = 0, column = 1, padx = 50, pady = 12.5)
+
         self.add_btn = tk.Button(self.file_frame, width = 5, height = 2, text = "Add", font = "10", bg = "#70db70")
-        self.add_btn.grid(row = 0, column = 1, padx = 50, pady = 12.5)
+        self.add_btn.grid(row = 0, column = 0, padx = 50, pady = 12.5)
 
         self.delete_btn = tk.Button(self.file_frame, width = 5, height = 2, text = "Delete", font = "10", bg = "#ff5c33")
         self.delete_btn.grid(row = 1, column = 1, padx = 50, pady = 12.5)
+
+    def make_btn_frame(self, root):
+        root.update()
+
+        self.btn_frame = tk.Frame(root, width = root.winfo_width(), height = root.winfo_height() * 0.3, bg = "grey")
+        self.btn_frame.pack(fill = tk.X, padx = 10, pady = 10, side=tk.BOTTOM)
+
+        self.prev_btn = tk.Button(self.btn_frame, text = "Previous image", font = "18")
+        self.prev_btn.grid(row=0, column=0, sticky=tk.E, padx = 50, pady = 12.5)
+
+        self.next_btn = tk.Button(self.btn_frame, text = "Next image", font = "18")
+        self.next_btn.grid(row=0, column=1, sticky=tk.W, padx = 50, pady = 12.5)
+
+        self.process_btn = tk.Button(self.btn_frame, text = "Process X-Ray", font = "24")
+        self.process_btn.grid(row=1, column=0, columnspan=2, sticky=tk.N + tk.E + tk.S + tk.W, padx = 50, pady = 12.5)
+
