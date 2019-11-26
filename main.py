20c75c27056c2f2bbcfc9dd8b3afefce551fbba8
warren <warrenb95@live.co.uk>
2019-11-26 12:49:32 +0000

    console menu

--- main.py
+++ main.py
@@ -5,24 +5,32 @@ from datetime import datetime
 
 if __name__ == "__main__":
 
-    # print(
-    #     "1. Launch the GUI application.\n"
-    #     "2. Train network"
-    # )
+    print(
+        "1. Launch the GUI application. (1)\n"
+        "2. Train classification (2)\n"
+        "3. Train regression ('elbow', 'finger', 'forearm', 'hand', 'humerus', 'shoulder', 'wrist')\n"
+    )
 
-    # usr_in = input("Please select: ")
+    usr_in = input("Please select: ")
 
-    # if int(usr_in) == 1:
-    #     controller = Controller()
-    #     controller.run()
-    # elif int(usr_in) == 2:
-
-    #     model = trainer.train_new_classification()
-
-    # calssification_trainer.train_new()
-
-    # model = regression_trainer.train_new_regression()
-    # regression_trainer.validate(model)
-
-    controller = Controller()
-    controller.run()
+    if int(usr_in) == 1:
+        controller = Controller()
+        controller.run()
+    elif int(usr_in) == 2:
+        model = classification_trainer.train_old()
+    elif usr_in == 'elbow':
+        model = regression_trainer.train_old('elbow')
+    elif usr_in == 'finger':
+        model = regression_trainer.train_old('finger')
+    elif usr_in == 'forearm':
+        model = regression_trainer.train_old('forearm')
+    elif usr_in == 'hand':
+        model = regression_trainer.train_old('hand')
+    elif usr_in == 'humerus':
+        model = regression_trainer.train_old('humerus')
+    elif usr_in == 'shoulder':
+        model = regression_trainer.train_old('shoulder')
+    elif usr_in == 'wrist':
+        model = regression_trainer.train_old('wrist')
+    else:
+        print("Invalid option... Bye")
