98d50b9f9b58b38b11f4638272f1c266294ffe0a
warren <warrenb95@live.co.uk>
2019-12-06 07:56:25 +0000

    added a testing section

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from keras import backend as k_back
 import os
 
 if __name__ == "__main__":
+    # Testing section
+    # ---------------------------------------------------------------------
+    model = regression_trainer.train_new('elbow')
+    # ---------------------------------------------------------------------
 
     # Uncomment to run the GUI
     # ---------------------------------------------------------------------
@@ -15,21 +19,21 @@ if __name__ == "__main__":
 
     # Uncomment to train the regression models
     # ---------------------------------------------------------------------
-    model_list = ['elbow', 'finger', 'forearm', 'hand', 'humerus', 'shoulder', 'wrist']
-    for m in model_list:
-        model = regression_trainer.train_old(m)
-        model = None
-        k_back.clear_session()
+    # model_list = ['elbow', 'finger', 'forearm', 'hand', 'humerus', 'shoulder', 'wrist']
+    # for m in model_list:
+    #     model = regression_trainer.train_old(m)
+    #     model = None
+    #     k_back.clear_session()
     # ---------------------------------------------------------------------
 
     # Uncomment to train classification model
     # ---------------------------------------------------------------------
-    model = classification_trainer.train_old()
+    # model = classification_trainer.train_old()
     # ---------------------------------------------------------------------
 
     # ---------------------------------------------------------------------
     # ---------------------------------------------------------------------
     # This will turn the PC off, use when training overnight
-    os.system("shutdown /s /t 1")
+    # os.system("shutdown /s /t 1")
     # ---------------------------------------------------------------------
     # ---------------------------------------------------------------------
