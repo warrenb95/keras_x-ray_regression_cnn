5317333e5cbf8b97d01a921b5f883461c736b2fe
warren <warrenb95@live.co.uk>
2020-01-03 17:07:48 +0000

    Training with desnet

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import keras
 
 TESTING = False
-batch_size = 25
-epochs = 100
+batch_size = 15
+epochs = 50
 classification_opt = keras.optimizers.SGD(learning_rate=0.001, momentum=0.9, nesterov=True)
 regression_opt = keras.optimizers.Nadam(learning_rate=0.001, beta_1=0.9, beta_2=0.999)
