a8813d7d343f7e1e942f0ab30246351c34cf97ee
warren <warrenb95@live.co.uk>
2020-02-18 16:05:47 +0000

    batch size 15 -> 10 to lower resource usage

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import tensorflow
 
 TESTING = False
-batch_size = 15
+batch_size = 10
 epochs = 100
 classification_opt = tensorflow.optimizers.SGD(learning_rate=0.001, momentum=0.9, nesterov=True)
 regression_opt = tensorflow.optimizers.Nadam(learning_rate=0.001, beta_1=0.9, beta_2=0.999)
