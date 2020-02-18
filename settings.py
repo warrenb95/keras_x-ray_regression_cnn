5ce4e0267673c24e8e91611cc850c4268ca4f181
warren <warrenb95@live.co.uk>
2020-02-18 16:03:09 +0000

    100 epochs

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import tensorflow
 
 TESTING = False
 batch_size = 15
-epochs = 50
+epochs = 100
 classification_opt = tensorflow.optimizers.SGD(learning_rate=0.001, momentum=0.9, nesterov=True)
 regression_opt = tensorflow.optimizers.Nadam(learning_rate=0.001, beta_1=0.9, beta_2=0.999)
