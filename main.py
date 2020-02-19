f9104b6f55e06d51524075c4b5f687bf949cd977
warren <warrenb95@live.co.uk>
2020-02-19 09:29:10 +0000

    100 epochs /w extended resnet152 model

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ if __name__ == "__main__":
 
     # Uncomment to train the regression models
     # ---------------------------------------------------------------------
-    # model_list = ['elbow', 'finger', 'forearm', 'hand', 'humerus', 'shoulder', 'wrist']
-    # for m in model_list:
-    #     model = regression_trainer.train_new(m)
-    #     model = None
-    #     k_back.clear_session()
+    model_list = ['elbow', 'finger', 'forearm', 'hand', 'humerus', 'shoulder', 'wrist']
+    for m in model_list:
+        model = regression_trainer.train_new(m)
+        model = None
+        k_back.clear_session()
     # ---------------------------------------------------------------------
 
     # Uncomment to train classification model
