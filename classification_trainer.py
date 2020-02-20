704ab79713c49398f907f306486736a750bf53b2
warren <warrenb95@live.co.uk>
2020-02-20 18:04:07 +0000

    Fixed sigleton classes

--- classification_trainer.py
+++ classification_trainer.py
@@ -7,134 +7,143 @@ import cv2
 import typing
 
 class Classification_Trainer:
+    class __Classification_Trainer:
 
-    def __init__(self):
-        self.model = None
-        self.trainer = None
+        def __init__(self):
+            self.model = None
+            self.trainer = None
 
-        # Settings.
-        # ---------------------------------------------------------------------
-        self.TESTING = settings.TESTING
-        self.batch_size = settings.batch_size
-        self.epochs = settings.epochs
-        self.opt = settings.classification_opt
-        # ---------------------------------------------------------------------
+            # Settings.
+            # ---------------------------------------------------------------------
+            self.TESTING = settings.TESTING
+            self.batch_size = settings.batch_size
+            self.epochs = settings.epochs
+            self.opt = settings.classification_opt
+            # ---------------------------------------------------------------------
 
-        # File variables.
-        # ---------------------------------------------------------------------
-        self.train_path = 'dataset/train'
-        self.valid_path = 'dataset/valid'
-        self.classification_train_total = 36808
-        self.classification_test_total = 3197
-        # ---------------------------------------------------------------------
+            # File variables.
+            # ---------------------------------------------------------------------
+            self.train_path = 'dataset/train'
+            self.valid_path = 'dataset/valid'
+            self.classification_train_total = 36808
+            self.classification_test_total = 3197
+            # ---------------------------------------------------------------------
 
-    def getInstance(self):
-        if self.trainer is None:
-            self.trainer = Classification_Trainer()
+        def getInstance(self):
+            if self.trainer is None:
+                self.trainer = Classification_Trainer()
 
-        return self.trainer
+            return self.trainer
 
-    def train_new(self):
-        '''Creates and train a new classification model.
-        '''
+        def train_new(self):
+            '''Creates and train a new classification model.
+            '''
 
-        self.model = helper_funcs.create_new_model(False, 7)
-        self.model.compile(loss='categorical_crossentropy', optimizer = self.opt, metrics = ['accuracy'])
+            self.model = helper_funcs.create_new_model(False, 7)
+            self.model.compile(loss='categorical_crossentropy', optimizer = self.opt, metrics = ['accuracy'])
 
-        train_classification_model()
+            train_classification_model()
 
-    def train_old(self):
-        '''Load in the classification model and train it.
-        '''
+        def train_old(self):
+            '''Load in the classification model and train it.
+            '''
 
-        self.model = helper_funcs.load_model('class')
-        self.model.compile(loss='categorical_crossentropy', optimizer = self.opt, metrics = ['accuracy'])
+            self.model = helper_funcs.load_model('class')
+            self.model.compile(loss='categorical_crossentropy', optimizer = self.opt, metrics = ['accuracy'])
 
-        train_classification_model()
+            train_classification_model()
 
-    def train_classification_model(self):
-        '''Set up and and train the model arg passed in.
+        def train_classification_model(self):
+            '''Set up and and train the model arg passed in.
 
-        Parameters
-        ----------
-        model: Sequential
-            A keras model to train
-        '''
+            Parameters
+            ----------
+            model: Sequential
+                A keras model to train
+            '''
 
-        classification_train_generator = ImageDataGenerator().flow_from_directory(self.train_path,
-                                                                                    target_size = (112, 112),
-                                                                                    batch_size = self.batch_size)
+            classification_train_generator = ImageDataGenerator().flow_from_directory(self.train_path,
+                                                                                        target_size = (112, 112),
+                                                                                        batch_size = self.batch_size)
 
-        classification_valid_generator = ImageDataGenerator().flow_from_directory(self.valid_path,
-                                                                                    target_size = (112, 112),
-                                                                                    batch_size = self.batch_size)
+            classification_valid_generator = ImageDataGenerator().flow_from_directory(self.valid_path,
+                                                                                        target_size = (112, 112),
+                                                                                        batch_size = self.batch_size)
 
-        if not self.TESTING:
+            if not self.TESTING:
 
-            try:
-                print("----------------- TRAINING -----------------")
-                history = self.model.fit_generator(classification_train_generator,
-                                    validation_data = classification_valid_generator,
+                try:
+                    print("----------------- TRAINING -----------------")
+                    history = self.model.fit_generator(classification_train_generator,
+                                        validation_data = classification_valid_generator,
+                                        steps_per_epoch = int(self.classification_train_total / self.batch_size),
+                                        validation_steps = int(self.classification_test_total / self.batch_size),
+                                        epochs = self.epochs,
+                                        verbose = 1,
+                                        shuffle = True)
+
+                    curr_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+                    # Plot training & validation data.
+                    plt.plot(history.history['accuracy'])
+                    plt.plot(history.history['val_accuracy'])
+                    plt.plot(history.history['loss'])
+                    plt.plot(history.history['val_loss'])
+                    plt.title('training data')
+                    plt.ylabel('Accuracy')
+                    plt.xlabel('Epoch')
+                    plt.legend(['accuracy', 'val_accuracy', 'loss', 'val_loss'], loc='upper left')
+
+                    fname = "model_graphs/" + curr_datetime + '_class.jpg'
+                    plt.savefig(fname)
+
+                    helper_funcs.save_model(model, 'class')
+
+                except KeyboardInterrupt:
+                    # Handle a keyboard interupts 'ctrl+c'
+                    helper_funcs.save_model(model, 'class')
+            else:
+                print("----------------- self.TESTING -----------------")
+                self.model.fit_generator(classification_train_generator,
                                     steps_per_epoch = int(self.classification_train_total / self.batch_size),
+                                    validation_data = classification_valid_generator,
                                     validation_steps = int(self.classification_test_total / self.batch_size),
                                     epochs = self.epochs,
                                     verbose = 1,
                                     shuffle = True)
 
-                curr_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        def predict_classification(self, image_path):
+            '''Predict image classification.
 
-                # Plot training & validation data.
-                plt.plot(history.history['accuracy'])
-                plt.plot(history.history['val_accuracy'])
-                plt.plot(history.history['loss'])
-                plt.plot(history.history['val_loss'])
-                plt.title('training data')
-                plt.ylabel('Accuracy')
-                plt.xlabel('Epoch')
-                plt.legend(['accuracy', 'val_accuracy', 'loss', 'val_loss'], loc='upper left')
+            Parameters
+            ----------
+            model: Sequential
+                Keras model
 
-                fname = "model_graphs/" + curr_datetime + '_class.jpg'
-                plt.savefig(fname)
+            image_path: str
+                The path of the image
 
-                helper_funcs.save_model(model, 'class')
+            Returns
+            -------
+            prediction_y: int
+                The prediction of the model
+            '''
 
-            except KeyboardInterrupt:
-                # Handle a keyboard interupts 'ctrl+c'
-                helper_funcs.save_model(model, 'class')
-        else:
-            print("----------------- self.TESTING -----------------")
-            self.model.fit_generator(classification_train_generator,
-                                steps_per_epoch = int(self.classification_train_total / self.batch_size),
-                                validation_data = classification_valid_generator,
-                                validation_steps = int(self.classification_test_total / self.batch_size),
-                                epochs = self.epochs,
-                                verbose = 1,
-                                shuffle = True)
+            if self.model is None:
+                self.model = helper_funcs.load_model('class')
 
-    def predict_classification(self, image_path):
-        '''Predict image classification.
+            self.model.compile(loss = 'categorical_crossentropy', optimizer = self.opt, metrics = ['accuracy'])
 
-        Parameters
-        ----------
-        model: Sequential
-            Keras model
+            cur_image = helper_funcs.load_single_image(image_path)
 
-        image_path: str
-            The path of the image
+            prediction_y = self.model.predict_classes(cur_image)
 
-        Returns
-        -------
-        prediction_y: int
-            The prediction of the model
-        '''
+            return prediction_y, cur_image
 
-        if self.model is None:
-            self.model = helper_funcs.load_model('class')
+    instance = None
 
-        self.model.compile(loss = 'categorical_crossentropy', optimizer = self.opt, metrics = ['accuracy'])
+    def getInstance(self):
+        if not Classification_Trainer.instance:
+            Classification_Trainer.instance = Classification_Trainer.__Classification_Trainer()
 
-        cur_image = helper_funcs.load_single_image(image_path)
-
-        prediction_y = self.model.predict_classes(cur_image)
-
-        return prediction_y, cur_image
+        return Classification_Trainer.instance
--- regression_trainer.py
+++ regression_trainer.py
@@ -15,157 +15,187 @@ import helper_funcs
 import settings
 
 class Regression_Trainer:
+    class __Regression_Trainer:
 
-    def __init__(self):
-        self.model = None
-        self.trainer = None
+        def __init__(self):
+            self.model = None
+            self.trainer = None
 
-        # Settings
-        # ---------------------------------------------------------------------
-        self.TESTING = settings.TESTING
-        self.batch_size = settings.batch_size
-        self.epochs = settings.epochs
-        self.opt = settings.regression_opt
-        # ---------------------------------------------------------------------
+            # Settings
+            # ---------------------------------------------------------------------
+            self.TESTING = settings.TESTING
+            self.batch_size = settings.batch_size
+            self.epochs = settings.epochs
+            self.opt = settings.regression_opt
+            # ---------------------------------------------------------------------
 
-        # File variables
-        # ---------------------------------------------------------------------
-        self.train_path = 'dataset/train'
-        self.valid_path = 'dataset/valid'
-        self.train_images_x_total = 0
-        self.test_images_x_total = 0
-        self.validation_images_x_total = 0
-        # ---------------------------------------------------------------------
+            # File variables
+            # ---------------------------------------------------------------------
+            self.train_path = 'dataset/train'
+            self.valid_path = 'dataset/valid'
+            self.train_images_x_total = 0
+            self.test_images_x_total = 0
+            self.validation_images_x_total = 0
+            # ---------------------------------------------------------------------
 
-    def getInstance(self):
-        if self.trainer is None:
-            self.trainer = Regression_Trainer()
+        def getInstance(self):
+            if self.trainer is None:
+                self.trainer = Regression_Trainer()
 
-        return self.trainer
+            return self.trainer
 
-    def load_regression_data(self, body_part):
-        '''Load the regression data and images for the 'body_part'.
+        def load_regression_data(self, body_part):
+            '''Load the regression data and images for the 'body_part'.
 
-        Parameters
-        ----------
-        body_part: str
-            The body part to load.
+            Parameters
+            ----------
+            body_part: str
+                The body part to load.
 
-        Returns
-        -------
-        train_images_x: [] cv2 image
-            A list of training images.
+            Returns
+            -------
+            train_images_x: [] cv2 image
+                A list of training images.
 
-        train_y: double
-            The target values of the train_images_x.
+            train_y: double
+                The target values of the train_images_x.
 
-        test_images_x: [] cv2 image
-            A list of training images.
+            test_images_x: [] cv2 image
+                A list of training images.
 
-        test_y: double
-            The target values of the test_y.
-        '''
+            test_y: double
+                The target values of the test_y.
+            '''
 
-        train_dataset_file = 'dataset/' + 'train_' + body_part + '.csv'
+            train_dataset_file = 'dataset/' + 'train_' + body_part + '.csv'
 
-        df = helper_funcs.load_dataset_attributes(train_dataset_file)
+            df = helper_funcs.load_dataset_attributes(train_dataset_file)
 
-        images = helper_funcs.load_images(df)
+            images = helper_funcs.load_images(df)
 
-        split = train_test_split(df,
-                                images,
-                                test_size=0.25,
-                                random_state=2,
-                                shuffle=True)
+            split = train_test_split(df,
+                                    images,
+                                    test_size=0.25,
+                                    random_state=2,
+                                    shuffle=True)
 
-        (train_attribs_x,
-        test_attribs_x,
-        train_images_x,
-        test_images_x) = split
+            (train_attribs_x,
+            test_attribs_x,
+            train_images_x,
+            test_images_x) = split
 
-        train_images_x = np.array(train_images_x)
-        test_images_x = np.array(test_images_x)
+            train_images_x = np.array(train_images_x)
+            test_images_x = np.array(test_images_x)
 
-        train_y = np.array(train_attribs_x['target'])
-        test_y = np.array(test_attribs_x['target'])
+            train_y = np.array(train_attribs_x['target'])
+            test_y = np.array(test_attribs_x['target'])
 
-        self.train_images_x_total = len(train_images_x)
-        self.test_images_x_total = len(test_images_x)
+            self.train_images_x_total = len(train_images_x)
+            self.test_images_x_total = len(test_images_x)
 
-        return (train_images_x, train_y, test_images_x, test_y)
+            return (train_images_x, train_y, test_images_x, test_y)
 
-    def train_new(self, body_part):
-        '''Create and train a new model for the 'body_part'.
+        def train_new(self, body_part):
+            '''Create and train a new model for the 'body_part'.
 
-        Parameters
-        ---------
-        body_part: str
-            The body part model to create and train.
+            Parameters
+            ---------
+            body_part: str
+                The body part model to create and train.
 
-        Returns
-        -------
-        model: Sequential
-            A keras model
-        '''
+            Returns
+            -------
+            model: Sequential
+                A keras model
+            '''
 
-        # model = helper_funcs.create_new_model(True, 0)
-        model = helper_funcs.create_desnet121()
-        model.compile(optimizer = self.opt, loss = 'mse')
-        return train_regression_model(model, body_part)
+            # model = helper_funcs.create_new_model(True, 0)
+            model = helper_funcs.create_desnet121()
+            model.compile(optimizer = self.opt, loss = 'mse')
+            return train_regression_model(model, body_part)
 
-    def train_old(self, body_part):
-        '''Load and train the 'body_part' model.
+        def train_old(self, body_part):
+            '''Load and train the 'body_part' model.
 
-        Parameters
-        ----------
-        body_part: str
-            The body part model to load and train.
+            Parameters
+            ----------
+            body_part: str
+                The body part model to load and train.
 
-        Returns
-        -------
-        model: Sequential
-            A keras model
-        '''
+            Returns
+            -------
+            model: Sequential
+                A keras model
+            '''
 
-        self.model = helper_funcs.load_model(body_part)
-        self.model.compile(optimizer = self.opt, loss = 'mse')
-        return train_regression_model(body_part)
+            self.model = helper_funcs.load_model(body_part)
+            self.model.compile(optimizer = self.opt, loss = 'mse')
+            return train_regression_model(body_part)
 
-    def train_regression_model(self, body_part):
-        ''' Set up and train the model for the body_part
+        def train_regression_model(self, body_part):
+            ''' Set up and train the model for the body_part
 
-        Parameters
-        ----------
-        model: Sequential
-            A keras model to train.
+            Parameters
+            ----------
+            model: Sequential
+                A keras model to train.
 
-        body_part: str
-            The body part model.
+            body_part: str
+                The body part model.
 
-        Returns
-        -------
-        model: Sequential
-            A keras model.
-        '''
+            Returns
+            -------
+            model: Sequential
+                A keras model.
+            '''
 
-        train_images_x, train_y, test_images_x, test_y = load_regression_data(body_part)
+            train_images_x, train_y, test_images_x, test_y = load_regression_data(body_part)
 
-        train_generator = ImageDataGenerator().flow(train_images_x, train_y, batch_size = self.batch_size)
-        test_generator = ImageDataGenerator().flow(test_images_x, test_y, batch_size = self.batch_size)
+            train_generator = ImageDataGenerator().flow(train_images_x, train_y, batch_size = self.batch_size)
+            test_generator = ImageDataGenerator().flow(test_images_x, test_y, batch_size = self.batch_size)
 
-        if not self.TESTING:
+            if not self.TESTING:
 
-            try:
-                print("----------------- TRAINING -----------------")
+                try:
+                    print("----------------- TRAINING -----------------")
+                    history = self.model.fit_generator(train_generator,
+                                        validation_data = test_generator,
+                                        steps_per_epoch = int(self.train_images_x_total / self.batch_size),
+                                        validation_steps = int(self.test_images_x_total / self.batch_size),
+                                        epochs = self.epochs,
+                                        verbose = 1,
+                                        shuffle = True)
+
+                    curr_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+                    # Plot training & validation data
+                    plt.plot(history.history['loss'])
+                    plt.plot(history.history['val_loss'])
+                    plt.title('training data')
+                    plt.ylabel('Loss')
+                    plt.xlabel('Epoch')
+                    plt.legend(['loss', 'val_loss'], loc='upper left')
+
+                    fname = "model_graphs/" + curr_datetime + '_' + body_part + '.jpg'
+                    plt.savefig(fname)
+                    plt.close()
+                    history = None
+
+                    helper_funcs.save_model(model, body_part)
+
+                except KeyboardInterrupt:
+                    helper_funcs.save_model(model, body_part)
+                else:
+                    helper_funcs.save_model(model, body_part)
+            else:
+                print("----------------- self.TESTING -----------------")
                 history = self.model.fit_generator(train_generator,
-                                    validation_data = test_generator,
-                                    steps_per_epoch = int(self.train_images_x_total / self.batch_size),
-                                    validation_steps = int(self.test_images_x_total / self.batch_size),
-                                    epochs = self.epochs,
-                                    verbose = 1,
-                                    shuffle = True)
-
-                curr_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+                                        validation_data = test_generator,
+                                        steps_per_epoch = int(self.train_images_x_total / self.batch_size),
+                                        validation_steps = int(self.test_images_x_total / self.batch_size),
+                                        epochs = self.epochs,
+                                        verbose = 1,
+                                        shuffle = True)
 
                 # Plot training & validation data
                 plt.plot(history.history['loss'])
@@ -175,72 +205,51 @@ class Regression_Trainer:
                 plt.xlabel('Epoch')
                 plt.legend(['loss', 'val_loss'], loc='upper left')
 
-                fname = "model_graphs/" + curr_datetime + '_' + body_part + '.jpg'
+                fname = "model_graphs/self.TESTING_" + body_part + '.jpg'
                 plt.savefig(fname)
                 plt.close()
                 history = None
 
-                helper_funcs.save_model(model, body_part)
+        def predict(self, cur_image, body_part):
+            '''Predict the abnormality of the image_path.
 
-            except KeyboardInterrupt:
-                helper_funcs.save_model(model, body_part)
-            else:
-                helper_funcs.save_model(model, body_part)
-        else:
-            print("----------------- self.TESTING -----------------")
-            history = self.model.fit_generator(train_generator,
-                                    validation_data = test_generator,
-                                    steps_per_epoch = int(self.train_images_x_total / self.batch_size),
-                                    validation_steps = int(self.test_images_x_total / self.batch_size),
-                                    epochs = self.epochs,
-                                    verbose = 1,
-                                    shuffle = True)
+            Parameters
+            ----------
+            image_path: str
+                The path of the image.
 
-            # Plot training & validation data
-            plt.plot(history.history['loss'])
-            plt.plot(history.history['val_loss'])
-            plt.title('training data')
-            plt.ylabel('Loss')
-            plt.xlabel('Epoch')
-            plt.legend(['loss', 'val_loss'], loc='upper left')
+            body_part: str
+                The body part model.
 
-            fname = "model_graphs/self.TESTING_" + body_part + '.jpg'
-            plt.savefig(fname)
-            plt.close()
-            history = None
+            Returns
+            -------
+            prediction: double
+                The prediction produced by the model.
+            '''
 
-    def predict(self, cur_image, body_part):
-        '''Predict the abnormality of the image_path.
+            self.model = helper_funcs.load_model(body_part)
 
-        Parameters
-        ----------
-        image_path: str
-            The path of the image.
+            self.model.compile(optimizer = self.opt, loss = 'mse')
 
-        body_part: str
-            The body part model.
+            prediction_y = self.model.predict(cur_image)
 
-        Returns
-        -------
-        prediction: double
-            The prediction produced by the model.
-        '''
+            prediction = prediction_y[0][0]
 
-        self.model = helper_funcs.load_model(body_part)
+            prediction *= 100
 
-        self.model.compile(optimizer = self.opt, loss = 'mse')
+            if prediction < 1:
+                prediction = 0.0
+            elif prediction > 100.0:
+                prediction = 100.0
 
-        prediction_y = self.model.predict(cur_image)
+            prediction = round(prediction, 1)
 
-        prediction = prediction_y[0][0]
+            return prediction
 
-        prediction *= 100
+    instance = None
 
-        if prediction < 1:
-            prediction = 0.0
-        elif prediction > 100.0:
-            prediction = 100.0
+    def getInstance(self):
+        if not Regression_Trainer.instance:
+            Regression_Trainer.instance = Regression_Trainer.__Regression_Trainer()
 
-        prediction = round(prediction, 1)
-
-        return prediction
+        return Regression_Trainer.instance
